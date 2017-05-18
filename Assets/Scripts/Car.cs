626e7ae6aae5761423e73f5190cbeea14cf3b153
Niklas Blom Laptop <blom.niklas@gmail.com>
2017-05-18 16:10:37 +0200

    CUBE CAR IS GO

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -11,6 +11,8 @@ public class Car : MonoBehaviour
     public float HandleRate;
     public float TopSpeed;
 
+    public float TopAccel = 2.0f;
+
     private float _acceleration;
     private float _velocity;
 
@@ -33,25 +35,50 @@ public class Car : MonoBehaviour
         else
         {
             // deaccel
-            _acceleration *= 0.95f;
-            _velocity *= 0.75f; // have a value [0,1] depending on current speed compared to top speed
+            _acceleration *= 0.99f;
+            _velocity *= 0.99f; // have a value [0,1] depending on current speed compared to top speed
         }
         if (Input.GetAxisRaw("Vertical") < -0.25f && !Input.GetButton("Jump"))
         {
-            Debug.Log("Holding down");
-            //_velocity *= 0.80f;
+            _velocity *= 0.80f;
             _acceleration *= 0.80f;
         }
 
+        if (_acceleration >= TopAccel)
+            _acceleration = TopAccel;
+
         var turning = Input.GetAxisRaw("Horizontal");
         // apply rotation
-        _transform.Rotate(_transform.up, turning);
+        _transform.Rotate(_transform.up, turning * HandleRate);
 
         if (_acceleration >= TopSpeed)
             _acceleration = TopSpeed;
 
         _velocity += _acceleration;
-
-        _transform.position += _transform.forward * _velocity * Time.deltaTime;
+        if (IsGrounded())
+        {
+            _transform.position += _transform.forward * _velocity * Time.deltaTime;
+            _rigidbody.position = _transform.position;
+        }
     }
+
+    // need to check input
+
+    // need to force car to ground (stronger gravity)
+
+    // need to move the car
+
+    // check if grounded
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(_transform.position, -_transform.up, out hit, 1.0f))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    // need a reset method
 }
