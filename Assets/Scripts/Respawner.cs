d70aaa8f3705497dac41876fdbf54df1f9484ee7
Anton Södergren <anton.sodergren@gmail.com>
2017-05-18 16:38:10 +0200

    Added basic respawner. (Road needs to be tagged Road and in layer Ground)

---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+public class Respawner : MonoBehaviour
+{
+    private Vector3 _spawnPosition;
+
+    public float RaycastDistance = 1f;
+    public LayerMask RoadMask;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        _spawnPosition = transform.position;
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, RaycastDistance, RoadMask))
+            if (hit.transform.CompareTag("Road"))
+                _spawnPosition = transform.position;
+    }
+
+
+    public void Respawn()
+    {
+        // Do some more stuff here
+
+        transform.position = _spawnPosition;
+    }
+}
