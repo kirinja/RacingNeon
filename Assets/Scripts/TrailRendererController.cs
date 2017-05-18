69cc4b50127024aece98b8ec5f102af9c2d950a4
Niklas Blom Laptop <blom.niklas@gmail.com>
2017-05-18 15:30:12 +0200

    Fan lagt in allt ifrån Unity

---
+++ Assets/Scripts/TrailRendererController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class TrailRendererController : MonoBehaviour
+{
+    public CarController CarController;
+    private TrailRenderer _renderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _renderer = GetComponent<TrailRenderer>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (CarController.CurrentSpeed > 0.0f)
+        {
+            _renderer.enabled = true;
+        }
+        else if (CarController.CurrentSpeed <= 0.01f)
+        {
+            _renderer.enabled = false;
+        }
+    }
+}
